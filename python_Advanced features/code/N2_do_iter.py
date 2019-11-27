ac9566dd7591ecf8fdc8b3be7a6d4fde9e8b4d4b
ITwxxrk <50392826+ITwxxrk@users.noreply.github.com>
2019-11-27 18:14:10 +0800

    Update N2_do_iter.py

--- python_Advanced features/code/N2_do_iter.py
+++ python_Advanced features/code/N2_do_iter.py
@@ -10,10 +10,10 @@ d = {'a':1,'b':2,'c':3}
 for key in d:
 	print(key)
 
-# 因为dict的存储不是按照list的方式顺序排列，所以，迭代出的结果顺序很可能不一样。
-# 默认情况下，dict迭代的是key。
-# 如果要迭代value，可以用for value in d.values()，
-# 如果要同时迭代key和value，可以用for k, v in d.items()。
+# 因为dict的存储不是按照list的方式顺序排列,所以,迭代出的结果顺序很可能不一样。
+# 默认情况下,dict迭代的是key。
+# 如果要迭代value,可以用for value in d.values()，
+# 如果要同时迭代key和value,可以用for k, v in d.items()。
 for value in d.values():
 	print(value)
 
